d4c0edc82a26be9a9138d1449c6cf7e306e5dc04
Beatriz Miranda <ext.beatriz.silva@farfetch.com>
2023-05-19 09:02:55 -0300

    feat: initial commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/health', async () => {
+  return '🎉 It is working!'
+})
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
